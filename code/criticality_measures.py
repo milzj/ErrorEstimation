38e1ccbcb0deb56e01ec38e9ca65e77fb8ebca5f
Johannes Milz <johannes.milz@isye.gatech.edu>
2024-02-08 12:28:24 -0500

    [jomi] Implemented some criticality measures.

---
+++ code/criticality_measures.py
@@ -0,0 +1,90 @@
+
+from prox import prox_box_l1
+import numpy as np
+
+class CriticalityMeasures(object):
+    """Evaluates several criticality measures
+
+    The proximal operator is computed using a composition formula.
+
+    Parameters:
+    -----------
+        x : ndarray or float
+            input array
+        lb, ub : ndarray or float
+            lower and upper bounds
+        g : ndarray or float
+            gradient evaluated at x
+        beta : float
+            regularization parameter, nonnegative
+        tau : float
+            criticality measure parameter, positive
+    """
+
+    def __init__(self,x,g,lb,ub,beta,tau=1.0):
+
+        self._x = x
+        self._g = g
+        self._lb = lb
+        self._ub = ub
+        self._beta = beta
+        self._tau = tau
+
+        self.canonical_residual
+        self.canonical_map
+
+        self.normal_residual
+        self.normal_map
+
+    @property
+    def canonical_residual(self):
+        """Evaluated the canonical residual
+
+        x - prox_{\psi/tau}(x-(1/tau)*g)
+
+        """
+
+        x = self._x
+        g = self._g
+        lb = self._lb
+        ub = self._ub
+        beta = self._beta
+        tau = self._tau
+
+        prox_v = prox_box_l1(x-(1/tau)*g, lb, ub, beta/tau)
+        self._canonical_residual = x - prox_v
+
+    @property
+    def canonical_map(self):
+        """Computes the 2-norm of the canonical residual."""
+        return np.linalg.norm(self._canonical_residual)
+
+    @property
+    def normal_residual(self, v=None):
+        """Evaluated the normal residual
+
+        tau*(v-prox(v)) + g(v).
+
+        If v is not supplied, the function computes
+        v according to v = x - (1/tau)*g and assumes that
+        g(x) = g(prox(v)).
+        """
+
+        x = self._x
+        g = self._g
+        lb = self._lb
+        ub = self._ub
+        beta = self._beta
+        tau = self._tau
+
+        if v == None:
+            v = x - (1/tau)*g
+
+        prox_v = prox_box_l1(v, lb, ub, beta/tau)
+
+        self._normal_residual = tau*(v-prox_v)+g
+
+    @property
+    def normal_map(self):
+        """Computes the 2-norm of the normal residual."""
+        return np.linalg.norm( self._canonical_residual)
---
+++ tests/test_criticality_measures.py
@@ -0,0 +1,28 @@
+import pytest
+
+import numpy as np
+from criticality_measures import CriticalityMeasures
+
+atol = 1e-12
+
+@pytest.mark.parametrize("n", [64, 128, 256])
+def test_criticality_measures(n):
+
+    lb = -np.ones(n)
+    ub = np.ones(n)
+    g = np.random.randn(n)
+    x = np.zeros(n)
+    beta = 1e-3
+
+    # min g^T x + beta norm(x, L1)
+
+    idx = g > beta
+    x[idx] = lb[idx]
+
+    idx = g < -beta
+    x[idx] = ub[idx]
+
+    cm = CriticalityMeasures(x, g, lb, ub, beta)
+
+    assert cm.normal_map < atol
+    assert cm.canonical_map < atol
