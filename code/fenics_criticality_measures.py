40f23b55188461c88f35b10cadffe20df981d714
Johannes Milz <johannes.milz@isye.gatech.edu>
2024-02-10 14:29:43 -0500

    [jomi] Added fenics version of criticality measures.

---
+++ code/fenics_criticality_measures.py
@@ -0,0 +1,36 @@
+from criticality_measures import CriticalityMeasures
+import fenics
+
+class FEniCSCriticalityMeasures(CriticalityMeasures):
+
+    def __init__(self,function_space,x,g,lb,ub,beta,tau=1.0):
+
+        _x = fenics.project(x, function_space)
+        _g = fenics.project(g, function_space)
+        _lb = fenics.project(lb, function_space)
+        _ub = fenics.project(ub, function_space)
+
+        x_vec = _x.vector().get_local()
+        g_vec = _g.vector().get_local()
+        lb_vec = _lb.vector().get_local()
+        ub_vec = _ub.vector().get_local()
+
+        self.function_space = function_space
+
+        super().__init__(x_vec, g_vec, lb_vec, ub_vec, beta, tau=tau)
+
+
+
+    @property
+    def canonical_map(self):
+        """Computes the L2-norm of the canonical residual."""
+        v = fenics.Function(self.function_space)
+        v.vector()[:] = self._canonical_residual
+        return fenics.norm(v, norm_type = "L2")
+
+    @property
+    def normal_map(self):
+        """Computes the L2-norm of the normal residual."""
+        v = fenics.Function(self.function_space)
+        v.vector()[:] = self._normal_residual
+        return fenics.norm(v, norm_type = "L2")
---
+++ tests/test_fenics_criticality_measures.py
@@ -0,0 +1,39 @@
+import pytest
+
+import numpy as np
+import fenics
+from fenics_criticality_measures import FEniCSCriticalityMeasures
+
+atol = 1e-14
+
+@pytest.mark.parametrize("n", [64, 128, 256])
+def test_fenics_criticality_measures(n):
+
+    mesh = fenics.UnitSquareMesh(n,n)
+    U = fenics.FunctionSpace(mesh, "DG", 0)
+
+    lb = fenics.Constant(-1.0)
+    ub = fenics.Constant(1.0)
+    g_vec = np.random.randn(U.dim())
+    g = fenics.Function(U)
+    g.vector()[:] = g_vec
+
+
+    x = np.zeros(U.dim())
+    beta = 1e-3
+
+    # min g^T x + beta norm(x, L1)
+
+    idx = g_vec > beta
+    x[idx] = -1.0
+
+    idx = g_vec < -beta
+    x[idx] = 1.0
+
+    u = fenics.Function(U)
+    u.vector()[:] = x
+
+    cm = FEniCSCriticalityMeasures(U, u, g, lb, ub, beta)
+
+    assert cm.normal_map < atol
+    assert cm.canonical_map < atol
