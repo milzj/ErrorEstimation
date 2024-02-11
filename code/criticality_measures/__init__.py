6279cba108dcbe1abb95b7d60043f57ff4cd1983
Johannes Milz <johannes.milz@isye.gatech.edu>
2024-02-10 21:10:41 -0500

    Fixing importing issues.

--- code/criticality_measures/__init__.py
+++
@@ -1,2 +0,0 @@
-from .fenics_criticality_measures import FEniCSCriticalityMeasures
-from .criticality_measures import CriticalityMeasures
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ import sys
 
 here = Path(__file__).parent
 sys.path.insert(0, str(here.parent) + "/code")
-sys.path.insert(0, str(here.parent) + "/code/criticality_measures")
 
 import pytest
 import numpy as np
