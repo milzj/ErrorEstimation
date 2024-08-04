ec1249c927eaa548aa08752c247146c349a651f4
Johannes Milz <60394891+milzj@users.noreply.github.com>
2024-08-04 18:15:06 -0400

    Update simulation_rates.sh

--- code/simulation_rates.sh
+++ code/simulation_rates.sh
@@ -4,6 +4,7 @@ declare -a Problems=("LinearProblem" "SemilinearProblem" "BilinearProblem")
 
 for P in "${Problems[@]}"
 do
+    echo $P
     mkdir -p output/$date/$P
     python simulation.py $date $P > output/$date/$P/simulation_terminal_output.txt
     python rates.py $date $P > output/$date/$P/rates_terminal_output.txt
